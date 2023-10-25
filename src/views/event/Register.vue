47e9b4b82e26c9a43d363d736f8ee56c3815c88a
fabio21777 <fabiosouza21777@gmail.com>
2023-10-25 20:58:35 -0300

    lição6- navegação programatica

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,8 +1,18 @@
 <script setup>
 import { defineProps } from 'vue'
+import { useRouter } from 'vue-router'
 
 defineProps(['event'])
+const router = useRouter()
+
+const register = () => {
+  // If registered then redirect to event details
+  router.push({
+    name: 'EventDetails',
+  })
+}
 </script>
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register Me!</button>
 </template>
