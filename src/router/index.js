3de1bdecbced489943781289e7107ddd26d20668
fabio21777 <fabiosouza21777@gmail.com>
2023-10-25 09:17:30 -0300

    l3-contruindo paginação

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '../views/EventListView.vue'
 import EventDetailsView from '../views/EventDetailsView.vue'
 import AboutView from '../views/AboutView.vue'
+import ExtraStuffView from '../views/ExtraStuff.view.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,7 @@ const router = createRouter({
       path: '/',
       name: 'event-list',
       component: EventListView,
+      props: (route) => ({ page: parseInt(route.query.page) || 1 }),
     },
     {
       path: '/event/:id',
@@ -21,6 +23,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/stuff',
+      name: 'stuff',
+      component: ExtraStuffView,
+      props: (route) => ({ showExtra: route.query.e }),
     },
   ],
 })
