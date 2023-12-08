0f5fcea45557b79df1e4491697972c3bb9624303
fabio21777 <fabiosouza21777@gmail.com>
2023-12-08 16:12:38 -0300

    vue form - baseRadio

---
+++ vue-form/Vue-3-Forms-l2-start/src/components/BaseRadio.vue
@@ -0,0 +1,31 @@
+<template lang="">
+  <input
+      type="radio"
+      :checked="modelValue === value"
+      :value="value"
+      @change="$emit('update:modelValue', value)"
+    />
+  <label v-if="label">{{ label }}</label>
+</template>
+<script>
+export default {
+
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number, Boolean],
+      default: ''
+    },
+    value: {
+      type: [String, Number, Boolean],
+      required: true
+    }
+  }
+}
+</script>
+<style lang="">
+
+</style>
