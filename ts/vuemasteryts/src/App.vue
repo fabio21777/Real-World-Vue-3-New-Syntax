d1ae75ebc98cb12971b53e21d0b20b07e2e3cf56
fabio21777 <fabiosouza21777@gmail.com>
2024-01-01 09:32:29 -0300

    l3

---
+++ ts/vuemasteryts/src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue'
+import fetchCount from './fetchCount' // a mock fetch function
+
+interface AppInfo {
+  name: string
+  slogan: string
+}
+
+const appInfo: AppInfo = reactive({
+  name: 'Counter',
+  slogan: 'an app you can count on'
+})
+
+const count = ref<number | null>(null)
+
+onMounted(() => {
+  fetchCount((initialCount) => {
+    count.value = initialCount
+  })
+})
+
+</script>
+
+<template>
+  <div>
+    <h1>{{ appInfo.name }}</h1>
+    <h2>{{ appInfo.slogan }}</h2>
+  </div>
+  <p>{{ count }}</p>
+</template>
